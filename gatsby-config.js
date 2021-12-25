115d17fc8c88f3cfcad107ae5271a847466db9f9
Richard Kim <richardskim111@gmail.com>
2021-12-24 19:56:27 -0500

    edit pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,9 @@ module.exports = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
     siteTitle: `Richard Kim`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
+    siteTitleAlt: `Richard Kim - Personal Blog`,
+    siteHeadline: `Ramblings of an aspie data scientist`,
+    siteUrl: `https://richardskim111.github.io/`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
@@ -33,11 +34,15 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/data_sensei`,
           },
           {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/richardskim111/`,
+          },
+          {
+            name: `Github`,
+            url: `https://github.com/richardskim111/`,
           },
         ],
       },
@@ -130,7 +135,7 @@ module.exports = {
               }
             `,
             output: `rss.xml`,
-            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+            title: `Richard Kim - Personal Blog`,
           },
         ],
       },
